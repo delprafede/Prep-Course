90cc733e397e5b7ea5873f060531b4b6ee44dc2f
federico del prado <delpradofederico0@gmail.com>
2022-04-17 22:22:00 -0300

    respondiendo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = hola;
+const nuevaString = 'toto';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 10;
@@ -30,33 +30,33 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str;
 }
 
-function suma(x, y) {(5 + 3
-)
-  // "x" e "y" son números
+function suma(x, y) {
+   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return (x / y);
+
 }
 
 function sonIguales(x, y) {
@@ -64,6 +64,8 @@ function sonIguales(x, y) {
   // De lo contrario, devuelve "false"
   // Tu código:
   
+  if (x === y); { return true;
+} { return false; 
 }
 
 function tienenMismaLongitud(str1, str2) {
